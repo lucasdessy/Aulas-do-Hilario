c0ec14c58ac39af5eda6b98199c0702ae32505d4
LadeiraA <62035575+ladeiraA@users.noreply.github.com>
2020-05-18 15:03:26 -0300

    Update Exercicio 17 Linguagem C.c

--- Exercicio 17 Linguagem C.c
+++ Exercicio 17 Linguagem C.c
@@ -5,7 +5,7 @@ int main(void) {
   printf("Digite o numero que deseja ver a tabuada\n");
     scanf("%f", &n);
 
-  for(int x = 1;x <= 10;x++){
+  for(int x = 0;x <= 10;x++){
     printf("%0.2f x %i = %0.2f \n", n, x,n*x);
   }
 }
